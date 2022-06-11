5d31a9128474572baa14b68d4db3594da9d953e3
chskela <chskela@gmail.com>
2022-06-11 19:10:12 +0300

    Added Extensions.kt

---
+++ app/src/main/java/com/chskela/sudoku/common/Extensions.kt
@@ -0,0 +1,33 @@
+package com.chskela.sudoku.common
+
+import android.app.Activity
+import android.widget.Toast
+import com.chskela.sudoku.R
+import com.chskela.sudoku.domain.Difficulty
+
+internal fun Activity.makeToast(message: String) {
+    Toast.makeText(
+        this,
+        message,
+        Toast.LENGTH_LONG
+    ).show()
+}
+
+internal fun Long.toTime(): String {
+    if (this >= 3600) return "+59:59"
+
+    var minutes = ((this % 3600) / 60).toString()
+    if (minutes.length == 1) minutes = "0$minutes"
+
+    var seconds = (this % 60).toString()
+    if (seconds.length == 1) seconds = "0$seconds"
+
+    return String.format("$minutes:$seconds")
+}
+
+internal val Difficulty.toLocalizedResource: Int
+    get() = when(this) {
+        Difficulty.EASY -> R.string.easy
+        Difficulty.MEDIUM -> R.string.medium
+        Difficulty.HARD -> R.string.hard
+    }
