2b7e844d63ce6637298eb7162a8200a2534db4e0
chskela <chskela@gmail.com>
2022-06-11 18:42:11 +0300

    Added IGameRepository.kt

---
+++ app/src/main/java/com/chskela/sudoku/domain/IGameRepository.kt
@@ -0,0 +1,40 @@
+package com.chskela.sudoku.domain
+
+interface IGameRepository {
+    suspend fun saveGame(
+        elapsedTime: Long,
+        onSuccess: (Unit) -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    suspend fun updateGame(
+        game: SudokuPuzzle,
+        onSuccess: (Unit) -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    suspend fun updateNote(
+        x: Int,
+        y: Int,
+        color: Int,
+        elapsedTime: Long,
+        onSuccess: (isComplete: Boolean) -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    suspend fun getCurrentGame(
+        onSuccess: (currentGame: SudokuPuzzle, isComplete: Boolean) -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    suspend fun getSettings(
+        onSuccess: (Settings) -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    suspend fun updateSettings(
+        settings: Settings,
+        onSuccess: (Unit) -> Unit,
+        onError: (Exception) -> Unit
+    )
+}
